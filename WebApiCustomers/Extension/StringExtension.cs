194083caa60b20b42790ce327ee45bb8402a33d9
Matheus Pitsch Warren <matheus.pitsch@warren.com.br>
2022-09-20 14:55:13 -0300

    [feat] changes

--- WebApiCustomers/Extension/StringExtension.cs
+++ WebApiCustomers/Extension/StringExtension.cs
@@ -8,48 +8,6 @@ namespace WebApiCustomers.Extension
         public static string CpfCorrect(this string cpf)
         {
             return cpf.Trim().Replace(".", "").Replace("-", "");
-        }
-
-        public static bool CheckCpf(this string cpf)
-        {
-            int[] multiplierOne = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            int[] multiplierTwo = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-
-            string temporaryCpf;
-            string Digit;
-            int rest;
-            int sum = 0;
-
-            cpf = cpf.Trim();
-            cpf = cpf.Replace(".", "").Replace("-", "");
-            if (cpf.Length != 11)
-                return false;
-
-            temporaryCpf = cpf.Substring(0, 9);
-
-            for (int i = 0; i < 9; i++)
-                sum += int.Parse(temporaryCpf[i].ToString()) * multiplierOne[i];
-            rest = sum % 11;
-
-            if (rest < 2)
-                rest = 0;
-            else
-                rest = 11 - rest;
-            Digit = rest.ToString();
-            temporaryCpf = temporaryCpf + Digit;
-            sum = 0;
-
-            for (int i = 0; i < 10; i++)
-                sum += int.Parse(temporaryCpf[i].ToString()) * multiplierTwo[i];
-            rest = sum % 11;
-
-            if (rest < 2)
-                rest = 0;
-            else
-                rest = 11 - rest;
-            Digit = Digit + rest.ToString();
-
-            return cpf.EndsWith(Digit);
         }        
     }
 }
--- WebApiCustomers/Validator/CustomerValidator.cs
+++ WebApiCustomers/Validator/CustomerValidator.cs
@@ -18,27 +18,21 @@ public class CustomerValidator : AbstractValidator<CustomersModel>
             .NotEmpty()
             .EmailAddress(EmailValidationMode.Net4xRegex)
             .Equal(v => v.EmailConfirmation);
-       
+
         RuleFor(c => c.Cpf)
             .NotEmpty()
-            .Must(c => c.CheckCpf())
+            .Must(CheckCpf)
                 .WithMessage("Cpf is not invalid");
 
         RuleFor(c => c.Cellphone)
             .NotEmpty()
             .MinimumLength(11)
-            .MaximumLength(16);                
+            .MaximumLength(16);
 
         RuleFor(c => c.DateOfBirth)
             .NotEmpty()
             .Must(c => c.checkEighteenMore())
                 .WithMessage("Date Of Birth must not be null or empty and can't have a date greater than today");
-
-        RuleFor(c => c.EmailSms)
-            .NotEmpty();
-
-        RuleFor(c => c.Whatsapp)
-            .NotEmpty();
 
         RuleFor(c => c.Country)
             .NotEmpty()
@@ -47,6 +41,7 @@ public class CustomerValidator : AbstractValidator<CustomersModel>
 
         RuleFor(c => c.City)
             .NotEmpty()
+            .MinimumLength(5)
             .MaximumLength(50);
 
         RuleFor(c => c.PostalCode)
@@ -61,7 +56,47 @@ public class CustomerValidator : AbstractValidator<CustomersModel>
 
         RuleFor(c => c.Number)
             .NotEmpty()
-            .MinimumLength(5)
-            .MaximumLength(50);
+            .GreaterThan(0);        
+    }
+    private bool CheckCpf(string cpf)
+    {
+        int[] multiplierOne = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] multiplierTwo = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        string temporaryCpf;
+        string Digit;
+        int rest;
+        int sum = 0;
+
+        cpf = cpf.Trim();
+        cpf = cpf.Replace(".", "").Replace("-", "");
+        if (cpf.Length != 11)
+            return false;
+
+        temporaryCpf = cpf.Substring(0, 9);
+
+        for (int i = 0; i < 9; i++)
+            sum += int.Parse(temporaryCpf[i].ToString()) * multiplierOne[i];
+        rest = sum % 11;
+
+        if (rest < 2)
+            rest = 0;
+        else
+            rest = 11 - rest;
+        Digit = rest.ToString();
+        temporaryCpf = temporaryCpf + Digit;
+        sum = 0;
+
+        for (int i = 0; i < 10; i++)
+            sum += int.Parse(temporaryCpf[i].ToString()) * multiplierTwo[i];
+        rest = sum % 11;
+
+        if (rest < 2)
+            rest = 0;
+        else
+            rest = 11 - rest;
+        Digit = Digit + rest.ToString();
+
+        return cpf.EndsWith(Digit);
     }
 }
